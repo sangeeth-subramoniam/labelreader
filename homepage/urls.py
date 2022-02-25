85aa73bfe3e55184b25709a539f2532d17d1297a
sangeeth-subramoniam <sangeethsubramoniam@gmail.com>
2022-02-25 17:46:17 +0900

    updated barcode and cover joined update

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from . import views
- 
+
 from django.conf import settings
 from django.conf.urls.static import static
- 
+
 app_name = 'homepage'
 urlpatterns = [
-    path('home', views.home , name = "home"),
+    path('', views.home , name = "home"),
+    path('camera', views.camera , name = "camera"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- homepage/views.py
+++ homepage/views.py
@@ -6,4 +6,21 @@ def home(request):
         curruser = request.user
         return render(request,'landing/homepage.html',{'curr_user' : curruser})
     else:
-        return render(request,'landing/homepage.html')
+        return redirect('authentication:signin')
+
+def camera(request):
+    if request.user.is_authenticated:
+        curruser = request.user
+
+        context = {
+            'cameraclick' : 1 ,
+            'FoodCheck' : 1 ,
+            'userid' : curruser.id,
+            'result_img1' : 'TEST/IMAGE/PATH'
+        }
+        
+        return render(request,'camera.html', context)
+    
+    else:
+
+        return redirect('authentication:signin')
